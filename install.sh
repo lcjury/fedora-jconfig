cd1b3fe13bd9b04e668e62f2a5e870fd6d363820
lcjury <zeui16@gmail.com>
2015-09-06 10:16:14 -0300

    vimrc added

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cp *.repo /etc/yum.repos.d/
 
 # rpmfusion repo
-su -c 'dnf localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm'
+su -c 'yum localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm'
 
 #update vim-minimal (fedora 20 conflict)
 #sudo yum update vim-minimal
@@ -11,19 +11,15 @@ su -c 'dnf localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/
 #install programs
 sudo dnf install gtk3-devel gtk2-devel intltool gnome-common tmux nano gcc google-chrome-stable nautilus-dropbox numix-gtk-theme numix-icon-theme numix-icon-theme-circle gcc-c++ vim-enhanced git-core unrar gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} ffmpeg
 
+#configure git
+git config --global user.name "lcjury"
+git config --global user.email zeui16@gmail.com
+
 #download solarized
 git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
 cd gnome-terminal-colors-solarized
 sh install.sh
 cd ..
-
-#vim solarized theme
-git clone https://github.com/altercation/vim-colors-solarized.git
-mkdir ~/.vim/
-mkdir ~/.vim/colors/
-cd vim-colors-solarized/colors
-mv solarized.vim ~/.vim/colors/
-cd ../../
 
 #init dropbox
 dropbox start -i &
@@ -47,11 +43,18 @@ make
 sudo cp -r extensions/alternate-tab /usr/share/gnome-shell/extensions/
 cd ../
 
-#configure git
-git config --global user.name "Luis Jury"
-git config --global user.email "zeui16@gmail.com"
 
 #Ruby on Rails
 sudo dnf install mysql-devel ruby-devel rubygems libxml2-devel libxslt-devel sqlite-devel nodejs
 gem install rails
 gem install sqlite3
+gem install heroku
+
+# Vim config
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp vimrc ~/.vimrc
+vim +PluginInstall +qall
+
+
+#Fonts
+dnf install google-droid-sans-mono-fonts
